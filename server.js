ae1fe07b59c0a055b9e84fa9999e83fa97592610
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-19 22:10:08 -0500

    Added mongoose MongoDB connection, created initial routes

--- server.js
+++ server.js
@@ -8,8 +8,24 @@ const db = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost/fitness', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/fitness', { useNewUrlParser: true });
+
+app.get("/", (res, rej) => {
+    res.status(200).sendFile("index.html");
+})
+
+app.post("/submit", (req, res) => {
+    const newRoutine = new db.Workout(req.body)
+    db.Workout.create(newRoutine)
+        .then(dbRoutine => {
+            res.status(200).send(dbRoutine);
+        })
+        .catch(err => {
+            res.status(200).send(`Workout already exists.`)
+        })
+});
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
