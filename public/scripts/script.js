ab982a964118ad277e3cc1f8021ee1cf5df148b5
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-19 22:57:58 -0500

    Server returns the names of all existing workouts

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,19 @@
 $("#loadWorkout").click(() => {
+    $.ajax({
+        url: "/workouts",
+        method: "GET",
+        success: result => {
+            console.log(result);
+        }
+    })
     $("#getFit").html(`
-    
+    <form action="/add" method="post">
+        <label for="exerciseName">Name of Exercise</label>
+        <input type="text" name="exerciseName" value="" placeholder="Exercise">
+        <label for="exerciseReps">Number of Reps</label>
+        <input type="number" name="exerciseReps" value="" placeholder="# of Reps">
+        <button class="btn btn-primary" id="addExercise">Add to Workout</button>
+    </form>
     `);
 });
 
@@ -10,7 +23,7 @@ $("#newWorkout").click(() => {
         <label for="workoutName">Name of Workout Routine</label>
         <input type="text" name="workoutName" value="" placeholder="Workout Title">
         <button class="btn btn-primary" id="createWorkout">Submit</button>
-    </form> 
+    </form>
     `);
     $("#createWorkout").click((event) => {
         event.preventDefault();
--- server.js
+++ server.js
@@ -12,11 +12,32 @@ app.use(express.static("public"));
 
 mongoose.connect('mongodb://localhost/fitness', { useNewUrlParser: true });
 
-app.get("/", (res, rej) => {
+app.get("/", (req, res) => {
     res.status(200).sendFile("index.html");
 })
 
+app.get("/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(dbRoutine => {
+            res.status(200).send(dbRoutine);
+        })
+        .catch(err => {
+            res.status(200).send("No data");
+        });
+})
+
 app.post("/submit", (req, res) => {
+    const newRoutine = new db.Workout(req.body)
+    db.Workout.create(newRoutine)
+        .then(dbRoutine => {
+            res.status(200).send(dbRoutine);
+        })
+        .catch(err => {
+            res.status(200).send(`Workout already exists.`)
+        })
+});
+
+app.post("/add", (req, res) => {
     const newRoutine = new db.Workout(req.body)
     db.Workout.create(newRoutine)
         .then(dbRoutine => {
