5ca9a78ce1ee6aafaa77a684757b7d37cbdd95f5
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-20 14:35:19 -0500

    Changed database calls to awaits, added more styling

--- controller/fitness_controller.js
+++ controller/fitness_controller.js
@@ -10,53 +10,36 @@ router.get("/", (req, res) => {
     res.status(200).sendFile("index.html");
 })
 
-router.get("/workouts", (req, res) => {
-    db.Workout.find({})
-        .then(dbRoutine => {
-            res.status(200).send(dbRoutine);
-        })
-        .catch(err => {
-            res.status(200).send("No data");
-        });
+try {
+router.get("/workouts", async (req, res) => {
+    let dbRoutine = await db.Workout.find({})
+    res.status(200).send(dbRoutine);
 })
 
-router.get("/populate/:workoutID", (req, res) => {
-    db.Workout.find({_id: req.params.workoutID})
-    .populate("exercises")
-    .then(dbRoutine => {
-        res.status(200).send(dbRoutine);
-    })
-        .catch(err => {
-        res.status(200).send(err);
-    });
+router.get("/populate/:workoutID", async (req, res) => {
+    let dbRoutine = await db.Workout.find({_id: req.params.workoutID}).populate("exercises")
+    res.status(200).send(dbRoutine);
 })
 
-router.post("/submit", (req, res) => {
+router.post("/submit", async (req, res) => {
     const newRoutine = new db.Workout(req.body)
-    db.Workout.create(newRoutine)
-        .then(dbRoutine => {
-            res.status(200).send(dbRoutine);
-        })
-        .catch(err => {
-            res.status(200).send(`Workout already exists.`)
-        })
+    let dbRoutine = await db.Workout.create(newRoutine)
+    res.status(200).send(dbRoutine);
 });
 
-router.post("/add", (req, res) => {
+router.post("/add", async (req, res) => {
     let exerciseInfo = {
         name: req.body.name,
         reps: req.body.reps
     }
     const newExercise = new db.Exercise(exerciseInfo)
-    db.Exercise.create(newExercise)
-        .then(({_id}) => 
-            db.Workout.findOneAndUpdate({_id: req.body.workout}, { $push: { exercises: _id } }, { new: true }))
-        .then(dbRoutine => {
-            res.status(200).send(dbRoutine);
-        })
-        .catch(err => {
-            res.status(200).send(`Invalid data.`)
-        })
+    let addExercise = await db.Exercise.create(newExercise)
+    let dbRoutine = await db.Workout.findOneAndUpdate({_id: req.body.workout}, { $push: { exercises: addExercise._id } }, { new: true })
+    res.status(200).send(dbRoutine);
 });
+
+} catch (err) {
+    console.log(`Failed server action. Error: ${err}`)
+}
 
 module.exports = router;
