7de154ea5685ff94552e981d2f290503406dc04c
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-20 01:28:13 -0500

    Created nested function to select a workout, then submit a new exercise to the database

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,18 +3,43 @@ $("#loadWorkout").click(() => {
         url: "/workouts",
         method: "GET",
         success: result => {
-            console.log(result);
+            $("#getFit").html("<h2>Choose a workout routine</h2>")
+            for (routine of result) {
+                $("#getFit").append(`
+                    <button class="btn btn-success workoutBtn" value="${routine._id}">${routine.name}</button>
+                `)
+            }
+            $(".workoutBtn").click(event => {
+                let workoutID = $(event.currentTarget).val();
+                $("#getFit").html(`
+                <h2>Add Exercise to ${$(event.currentTarget).text()}</h2>
+                <form action="/add" method="post">
+                    <label for="exerciseName">Name of Exercise</label>
+                    <input type="text" name="exerciseName" value="" placeholder="Exercise">
+                    <label for="exerciseReps">Number of Reps</label>
+                    <input type="number" name="exerciseReps" value="" placeholder="# of Reps">
+                    <button class="btn btn-primary" id="addExercise">Add to Workout</button>
+                </form>
+                `);
+                $("#addExercise").click((event) => {
+                    event.preventDefault();
+                    let newExercise = {
+                        workout: workoutID,
+                        name: $("input[name*='exerciseName']").val(),
+                        reps: $("input[name*='exerciseReps']").val()
+                    }
+                    $.ajax({
+                        url: "/add",
+                        data: newExercise,
+                        method: "POST",
+                        success: result => {
+                            console.log(result);
+                        }
+                    })
+                })
+            })
         }
     })
-    $("#getFit").html(`
-    <form action="/add" method="post">
-        <label for="exerciseName">Name of Exercise</label>
-        <input type="text" name="exerciseName" value="" placeholder="Exercise">
-        <label for="exerciseReps">Number of Reps</label>
-        <input type="number" name="exerciseReps" value="" placeholder="# of Reps">
-        <button class="btn btn-primary" id="addExercise">Add to Workout</button>
-    </form>
-    `);
 });
 
 $("#newWorkout").click(() => {
